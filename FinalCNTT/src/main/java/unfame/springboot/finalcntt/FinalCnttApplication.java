6dce2b484c5b75f0298f5401376b74445ebe57b4
Quốc Thắng <quocthang036@gmail.com>
2021-12-12 03:52:52 +0700

    Add BE
    Add BE

---
+++ FinalCNTT/src/main/java/unfame/springboot/finalcntt/FinalCnttApplication.java
@@ -0,0 +1,13 @@
+package unfame.springboot.finalcntt;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class FinalCnttApplication {
+
+    public static void main(String[] args) {
+        SpringApplication.run(FinalCnttApplication.class, args);
+    }
+
+}
