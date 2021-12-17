ab8b31d21ad0d1c5419cac312958d503ee7c6495
Quốc Thắng <quocthang036@gmail.com>
2021-12-17 21:31:02 +0700

    add full User, Admin, Account feature
    add full User, Admin, Account feature

--- FinalCNTT/src/main/java/unfame/springboot/finalcntt/repository/AccountRepository.java
+++ FinalCNTT/src/main/java/unfame/springboot/finalcntt/repository/AccountRepository.java
@@ -5,6 +5,8 @@ import org.springframework.data.repository.CrudRepository;
 import org.springframework.stereotype.Repository;
 import unfame.springboot.finalcntt.entity.Account;
 
+import java.util.List;
+
 @Repository
 public interface AccountRepository extends CrudRepository<Account, Long> {
     @Query("SELECT u FROM ACCOUNT u WHERE u.Id = ?1")
@@ -15,4 +17,7 @@ public interface AccountRepository extends CrudRepository<Account, Long> {
 
     @Query("SELECT u FROM ACCOUNT u WHERE u.Username = ?1 and u.Password = ?2")
     Account findAccountByUserNameAndPassword(String Username, String Password);
+
+    @Query("SELECT a, b FROM ACCOUNT a, USER b WHERE a.Id = ?1 and a.Id = b.Account_id")
+    List<Object[]> findAccountUsernameWithImage(Long Id);
 }
